bbf2b049dcd3b30a199a234dd0fb075764a44216
Alexander Schepanovski <suor.web@gmail.com>
2019-04-20 19:25:07 +0700

    Initial hacks

---
+++ README.md
@@ -0,0 +1,12 @@
+# KeylayReplay
+
+...
+
+
+## Installation
+
+<!-- TODO: fix -->
+- [Install Package Control](https://sublime.wbond.net/installation).
+- Bring up the Command Palette with Ctrl+Shift+p (Cmd+Shift+p on OS X).
+- Select "Package Control: Install Package" (it'll take a few seconds).
+- Select or type in "Keylay" when the list appears and press Enter.
---
+++ klreplay.py
@@ -0,0 +1,38 @@
+import re
+import sublime, sublime_plugin
+
+
+RU_CHARS = "ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ.йцукенгшщзхъфывапролджэячсмитьбю"
+EN_CHARS = "QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>/qwertyuiop[]asdfghjkl;'zxcvbnm,."
+
+RU_RE = re.compile(r'([%s\s]*)$' % re.escape(RU_CHARS))
+EN_RE = re.compile(r'([%s\s]*)$' % re.escape(EN_CHARS))
+
+TABLE = str.maketrans(RU_CHARS + EN_CHARS, EN_CHARS + RU_CHARS)
+
+
+class KeylayReplayCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        sel = self.view.sel()
+        if len(sel) == 1 and sel[0].empty():
+            pos = sel[0].a
+            line_s = line_start_str(self.view, pos)
+            ru_len = len(RU_RE.search(line_s).group())
+            en_len = len(EN_RE.search(line_s).group())
+            regions = [sublime.Region(pos - max(ru_len, en_len), pos)]
+        else:
+            regions = list(sel)
+
+        for region in regions:
+            text = self.view.substr(region)
+            self.view.replace(edit, region, text.translate(TABLE))
+
+
+# View tools
+
+def line_start(view, pos):
+    line = view.line(pos)
+    return sublime.Region(line.begin(), pos)
+
+def line_start_str(view, pos):
+    return view.substr(line_start(view, pos))
